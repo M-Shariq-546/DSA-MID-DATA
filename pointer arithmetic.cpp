bf05137be08b15c26862f88f5eb8f96eb8e929a3
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-09-06 11:16:03 +0500

    Pointer Arithmetic

---
+++ pointer arithmetic.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+const int len = 5;
+void incri(int arr[])
+{
+    int *ptr;
+    ptr = arr;
+    for(int i=0; i<len ; i++)
+    {
+        cout<<"The address of the : "<<ptr<<endl;
+        cout<<"The value is : "<<*ptr<<endl;
+        ptr++;
+    }
+    delete ptr;
+}
+void decri(int arr[len])
+{
+    int *ptr;
+    ptr = &arr[len -1];
+    for(int i=len; i>0 ; i--)
+    {
+        cout<<"The address of the : "<<ptr<<endl;
+        cout<<"The value is : "<<*ptr<<endl;
+        ptr--;
+    }
+    delete ptr;
+}
+int main()
+{
+    int arr[len] = {1 ,2  , 3, 4 , 5};
+    cout<<"The Increment of pointer "<<endl;
+    incri(arr);
+    cout<<"The Decrement of pointer "<<endl;
+    decri(arr);
+    return 0;
+}
